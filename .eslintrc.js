0e9c17ee08b1881993162fafa745cb9eafb815d9
poeynus <poeynus@gmail.com>
2022-01-16 20:20:00 +0900

    스토리 북 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,29 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'airbnb', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   rules: {
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx', 'plugin:storybook/recommended'],
+      },
+    ],
     'import/no-named-as-default': 0,
     'import/no-named-as-default-member': 0,
     'import/prefer-default-export': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
   parser: '@babel/eslint-parser',
   parserOptions: {
--- README.md
+++ README.md
@@ -13,3 +13,14 @@
 2. 역할 분담
    - 김정수: 회원 가입
    - 이선엽: 로그인
+
+## 22.01.16
+
+1. React Design pattern 선택
+   - 근본의 presentational - container 사용하려다 생각이 바뀌었다.
+   - [presentational - container를 사용하지 않는 이유](https://tecoble.techcourse.co.kr/post/2021-04-26-presentational-and-container/)
+   - class 형과 fun 형은 큰 차이가 있고 그에 적절한 것을 사용해야 한다.
+2. Storybook 설치
+   - 상당히 유용한 라이브러리, 새로운 걸 적용하면 에러가 너무 많이 떠
+   - [eslint와 storybook의 충돌](https://stackoverflow.com/questions/69928061/struggling-with-typescript-react-eslint-and-simple-arrow-functions-components)
+   - [Storybook](https://storybook.js.org/tutorials/intro-to-storybook/react/ko/get-started/)
