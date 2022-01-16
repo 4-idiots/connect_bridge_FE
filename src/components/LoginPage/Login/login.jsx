5cbb4775262437c1d2288fb91a593c8e82966655
poeynus <poeynus@gmail.com>
2022-01-17 00:04:56 +0900

    로그인 페이지 디자인을 제외하고 완성

--- src/components/LoginPage/Login/login.jsx
+++ src/components/LoginPage/Login/login.jsx
@@ -1,6 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import axios from 'axios';
 import * as S from './style';
 
 export const LoginForm = () => {
-  return <S.title>login</S.title>;
+  const [account, setAccount] = useState({
+    userID: '',
+    userPW: '',
+  });
+
+  const { userID, userPW } = account;
+
+  const onChangeAccountEvent = e => {
+    setAccount({
+      ...account,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const onSubmitEvent = e => {
+    e.preventDefault();
+    console.log(account);
+    // axios.post('', { userID, userPW }).then(response => {
+    //   console.log(response);
+    // });
+  };
+
+  return (
+    <S.contatiner>
+      <S.title>Login</S.title>
+      <S.form>
+        <S.input
+          type="text"
+          value={userID}
+          name="userID"
+          onChange={onChangeAccountEvent}
+          placeholder="아이디"
+        />
+        <S.input
+          type="password"
+          value={userPW}
+          name="userPW"
+          onChange={onChangeAccountEvent}
+          placeholder="비밀번호"
+        />
+        <S.submit onClick={onSubmitEvent}>Login</S.submit>
+      </S.form>
+      <S.serviceBox>
+        <S.serviceBtn service="register">회원가입</S.serviceBtn>
+        <S.serviceBtn service="findID">아이디 찾기</S.serviceBtn>
+        <S.serviceBtn service="findPW">비밀번호 찾기</S.serviceBtn>
+      </S.serviceBox>
+    </S.contatiner>
+  );
 };
--- src/components/LoginPage/Login/style.js
+++ src/components/LoginPage/Login/style.js
@@ -1,5 +1,33 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const contatiner = styled.div``;
 
 export const title = styled.h2`
   color: green;
+`;
+
+export const form = styled.form``;
+
+export const input = styled.input``;
+
+export const submit = styled.button``;
+
+export const serviceBox = styled.div``;
+
+export const serviceBtn = styled.button`
+  ${props =>
+    props.service === 'register' &&
+    css`
+      color: red;
+    `}
+  ${props =>
+    props.service === 'findID' &&
+    css`
+      color: green;
+    `}
+  ${props =>
+    props.service === 'indPW' &&
+    css`
+      color: blue;
+    `}
 `;
