9f0a3d2aa19906704b68479e88a6c939aeeb4a06
poeynus <poeynus@gmail.com>
2022-01-24 18:51:34 +0900

    아이디 찾기 페이지 구현 완료

--- src/components/LoginPage/FindID/findID.jsx
+++ src/components/LoginPage/FindID/findID.jsx
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 
 export const FindIDForm = () => {
-  return <S.title>findID</S.title>;
+  const [userInfo, setUserInfo] = useState({
+    userName: '',
+    userPhone: '',
+    userEmail: '',
+  });
+
+  const { userName, userPhone, userEmail } = userInfo;
+
+  const onChangeAccountEvent = e => {
+    setUserInfo({
+      ...userInfo,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const onSubmitEvent = e => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
+
+  return (
+    <S.contatiner>
+      <S.title>아이디 찾기</S.title>
+      <S.form>
+        <S.label>
+          이름
+          <S.input
+            type="text"
+            value={userName}
+            name="userName"
+            onChange={onChangeAccountEvent}
+            placeholder="이름"
+          />
+        </S.label>
+        <S.label>
+          휴대폰 번호
+          <S.input
+            type="text"
+            value={userPhone}
+            name="userPhone"
+            onChange={onChangeAccountEvent}
+            placeholder="ex) 01012345678"
+          />
+        </S.label>
+        <S.label>
+          이메일
+          <S.input
+            type="email"
+            value={userEmail}
+            name="userEmail"
+            onChange={onChangeAccountEvent}
+            placeholder="이메일"
+          />
+        </S.label>
+        <S.submit onClick={onSubmitEvent}>아이디 찾기</S.submit>
+      </S.form>
+    </S.contatiner>
+  );
 };
--- src/components/LoginPage/Login/login.jsx
+++ src/components/LoginPage/Login/login.jsx
@@ -27,28 +27,40 @@ export const LoginForm = () => {
 
   return (
     <S.contatiner>
-      <S.title>Login</S.title>
+      <S.title>로그인</S.title>
       <S.form>
-        <S.input
-          type="text"
-          value={userID}
-          name="userID"
-          onChange={onChangeAccountEvent}
-          placeholder="아이디"
-        />
-        <S.input
-          type="password"
-          value={userPW}
-          name="userPW"
-          onChange={onChangeAccountEvent}
-          placeholder="비밀번호"
-        />
-        <S.submit onClick={onSubmitEvent}>Login</S.submit>
+        <S.label>
+          아이디
+          <S.input
+            type="text"
+            value={userID}
+            name="userID"
+            onChange={onChangeAccountEvent}
+            placeholder="아이디"
+          />
+        </S.label>
+        <S.label>
+          비밀번호
+          <S.input
+            type="password"
+            value={userPW}
+            name="userPW"
+            onChange={onChangeAccountEvent}
+            placeholder="비밀번호"
+          />
+        </S.label>
+        <S.submit onClick={onSubmitEvent}>로그인</S.submit>
       </S.form>
       <S.serviceBox>
-        <S.serviceBtn service="register">회원가입</S.serviceBtn>
-        <S.serviceBtn service="findID">아이디 찾기</S.serviceBtn>
-        <S.serviceBtn service="findPW">비밀번호 찾기</S.serviceBtn>
+        <S.serviceBtn service="register" to="/signup">
+          회원가입
+        </S.serviceBtn>
+        <S.serviceBtn service="findID" to="/login/findID">
+          아이디 찾기
+        </S.serviceBtn>
+        <S.serviceBtn service="findPW" to="/login/findPW">
+          비밀번호 찾기
+        </S.serviceBtn>
       </S.serviceBox>
     </S.contatiner>
   );
