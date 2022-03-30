9ac2ad8d34d84065eef512896eff7fcfa1a200c0
poeynus <poeynus@gmail.com>
2022-03-30 22:45:25 +0900

    Dev project detail 95%

--- src/components/ProjectPage/detailComponent/style.js
+++ src/components/ProjectPage/detailComponent/style.js
@@ -1,8 +1,8 @@
 import styled, { css } from 'styled-components';
 
-export const HeaderWrap = styled.div``;
-
-export const PurposeWrap = styled.div``;
+export const HeaderWrap = styled.div`
+  padding: 40px 0 44px;
+`;
 
 export const HeaderContent = styled.div`
   display: flex;
@@ -45,21 +45,21 @@ export const StatusBox = styled.div`
 `;
 
 export const PageWrap = styled.div`
+  margin-left: 100px;
+  max-width: 1200px;
+  width: 100%;
   display: flex;
-  justify-content: space-between;
   flex-direction: row;
-`;
-
-export const PageLeft = styled.div`
-  max-width: 840px;
-  box-sizing: border-box;
+  justify-content: space-between;
 `;
 
 export const PageRight = styled.div`
   max-width: 300px;
+  max-height: 600px;
   width: 100%;
   border: 1px solid #ddd;
   padding: 20px;
+  border-radius: 5px;
 `;
 
 export const RightInfo = styled.div`
@@ -87,3 +87,112 @@ export const RightPBig = styled.p`
 export const RightPSmall = styled.p`
   font-size: 1rem;
 `;
+
+export const PageLeft = styled.div`
+  max-width: 840px;
+  box-sizing: border-box;
+  padding: 0 0 40px;
+  width: 100%;
+`;
+
+export const LeftTab = styled.div`
+  margin-bottom: 15px;
+  width: 100%;
+`;
+
+export const TalUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const TabLi = styled.li`
+  font-size: 15px;
+  color: #42495b;
+  padding: 12px;
+  border: 1px solid #000;
+`;
+
+export const LeftDetail = styled.div`
+  display: block;
+`;
+
+export const DetailStatus = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  box-sizing: border-box;
+  padding: 15px 24px 15px 0;
+  margin-bottom: 35px;
+`;
+
+export const StatusUl = styled.ul`
+  margin-top: 16px;
+`;
+
+export const StatusLi = styled.li`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 8px;
+  align-items: center;
+`;
+
+export const StatusBigP = styled.p`
+  color: #424251;
+  margin-right: 30px;
+  width: 221px;
+  font-weight: bold;
+`;
+
+export const StatusSmallP = styled.p`
+  margin-right: 34px;
+  width: 20px;
+`;
+
+export const DetailPlatform = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 35px;
+  box-sizing: border-box;
+  padding: 15px 24px 15px 0;
+`;
+
+export const PlatformWrap = styled.div`
+  display: flex;
+`;
+
+export const PlatformText = styled.div`
+  margin-right: 10px;
+`;
+
+export const DetailContent = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 35px;
+  box-sizing: border-box;
+  padding: 15px 24px 15px 0;
+`;
+
+export const SkillWrap = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 35px;
+  box-sizing: border-box;
+  padding: 15px 24px 15px 0;
+`;
+
+export const SkillBox = styled.div`
+  display: flex;
+`;
+
+export const ReferenceWrap = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 35px;
+  box-sizing: border-box;
+  padding: 15px 24px 15px 0;
+`;
+
+export const CommentWrap = styled.div`
+  padding: 15px 24px 15px 0;
+`;
+
+export const CustomFormWrap = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  margin-bottom: 35px;
+`;
+
+export const MediaBox = styled.div``;
--- src/components/ProjectPage/projectDetailModule.jsx
+++ src/components/ProjectPage/projectDetailModule.jsx
@@ -9,11 +9,40 @@ import {
   Content,
   Icon,
   Button,
+  Notification,
+  Block,
+  Form,
 } from 'react-bulma-components';
+import ReadOnlySlate from '../../SlateEditor/ReadOnly';
 import * as S from './detailComponent/style';
 
 export const ProjectDetailForm = () => {
   const { projectID } = useParams();
+  const [isInfo, setIsInfo] = useState(true);
+  const [comment, setComment] = useState('');
+  const [test, setTest] = useState({
+    projectContent: [
+      {
+        type: 'paragaph',
+        children: [
+          { text: '프로젝트를 소' },
+          { text: '개 하자면....asdasd', bold: true },
+        ],
+      },
+      {
+        type: 'paragaph',
+        children: [
+          { text: 'a', color: '#a10000', bold: true },
+          { color: '#a10000', text: 'sdasd' },
+        ],
+      },
+      { type: 'paragaph', children: [{ text: 'asdasd', color: '#a10000' }] },
+      { type: 'blockquote', children: [{ text: 'asdas' }] },
+      { type: 'paragaph', children: [{ text: 'das', underline: true }] },
+      { type: 'paragaph', children: [{ text: 'dasd' }] },
+      { type: 'paragaph', children: [{ text: 'asd' }] },
+    ],
+  });
 
   return (
     <Container>
@@ -22,7 +51,7 @@ export const ProjectDetailForm = () => {
           <S.ProjectContent>사이드프로젝트</S.ProjectContent>
           <Heading
             size={4}
-            style={{ fontWeight: 'bold', fontSize: 34, margin: 24 }}
+            style={{ fontWeight: 'bold', fontSize: 38, margin: 24 }}
           >
             웹소설 웹툰 IP 웹 3.0
           </Heading>
@@ -42,7 +71,146 @@ export const ProjectDetailForm = () => {
       </S.HeaderWrap>
 
       <S.PageWrap>
-        <S.PageLeft>asd</S.PageLeft>
+        <S.PageLeft>
+          <S.LeftTab>
+            <S.TalUl>
+              <S.TabLi onClick={() => setIsInfo(true)}>정보</S.TabLi>
+              <S.TabLi onClick={() => setIsInfo(false)}>질문</S.TabLi>
+            </S.TalUl>
+          </S.LeftTab>
+          {isInfo && (
+            <S.LeftDetail>
+              <S.DetailStatus>
+                <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                  모집 현황
+                </Heading>
+                <S.StatusUl>
+                  <S.StatusLi>
+                    <S.StatusBigP>프로젝트 매니저</S.StatusBigP>
+                    <S.StatusSmallP>1/1</S.StatusSmallP>
+                    <Button color="danger">완료</Button>
+                  </S.StatusLi>
+                  <S.StatusLi>
+                    <S.StatusBigP>UI/UX디자인</S.StatusBigP>
+                    <S.StatusSmallP>0/1</S.StatusSmallP>
+                    <Button color="info">신청</Button>
+                  </S.StatusLi>
+                  <S.StatusLi>
+                    <S.StatusBigP>웹 서버</S.StatusBigP>
+                    <S.StatusSmallP>0/1</S.StatusSmallP>
+                    <Button color="info">신청</Button>
+                  </S.StatusLi>
+                  <S.StatusLi>
+                    <S.StatusBigP>크로스플랫폼</S.StatusBigP>
+                    <S.StatusSmallP>0/1</S.StatusSmallP>
+                    <Button color="info">신청</Button>
+                  </S.StatusLi>
+                </S.StatusUl>
+              </S.DetailStatus>
+              <S.DetailPlatform>
+                <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                  출시 플랫폼
+                </Heading>
+                <S.PlatformWrap>
+                  <S.PlatformText>반응형 웹(PC/모바일)</S.PlatformText>
+                  <S.PlatformText>안드로이드 앱</S.PlatformText>
+                  <S.PlatformText>PC(윈도우/맥) 프로그램</S.PlatformText>
+                </S.PlatformWrap>
+              </S.DetailPlatform>
+              <S.DetailContent>
+                <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                  소개
+                </Heading>
+                <ReadOnlySlate value={test.projectContent} />
+              </S.DetailContent>
+              <S.SkillWrap>
+                <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                  기술/언어
+                </Heading>
+                <S.SkillBox>
+                  <Tag color="dark" style={{ marginRight: 10 }} rounded>
+                    React
+                  </Tag>
+                  <Tag color="dark" style={{ marginRight: 10 }} rounded>
+                    Java
+                  </Tag>
+                </S.SkillBox>
+              </S.SkillWrap>
+              <S.ReferenceWrap>
+                <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                  참고 링크
+                </Heading>
+                <Block>
+                  <Notification style={{ cursor: 'pointer' }}>
+                    http://naver.com
+                  </Notification>
+                </Block>
+              </S.ReferenceWrap>
+            </S.LeftDetail>
+          )}
+          {!isInfo && (
+            <S.CommentWrap>
+              <Heading size={7} style={{ fontWeight: 'bold', fontSize: 26 }}>
+                👍 이 모임에 응원 * 질문을 올려주세요!
+              </Heading>
+              <S.CustomFormWrap>
+                <Form.Field>
+                  <Form.Textarea
+                    placeholder="이 모임에 응원 * 질문을 올려주세요!"
+                    size="medium"
+                    value={comment}
+                    onChange={e => setComment(e.currentTarget.value)}
+                    style={{
+                      marginBottom: '20px',
+                    }}
+                  />
+                </Form.Field>
+              </S.CustomFormWrap>
+              <S.MediaBox>
+                <Media>
+                  <Media.Item align="left">
+                    <Image
+                      size={96}
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyGnV38qI3kabyXoa6e9eOn9960Lcnzj3jGA&usqp=CAU"
+                    />
+                  </Media.Item>
+                  <Media.Item align="center">
+                    <Content>
+                      <p>
+                        <strong>amateur</strong>
+                        <br />
+                        잘 몰라도 괜찮나요?
+                        <br />
+                        <small>2020-03-29</small>
+                      </p>
+                    </Content>
+
+                    <Media>
+                      <Media.Item align="left">
+                        <Image
+                          size={96}
+                          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScy0G31HTq6ikRm8C_nGlHb9yeOTLi4GgQNw&usqp=CAU"
+                        />
+                      </Media.Item>
+                      <Media.Item align="center">
+                        <Content>
+                          <p>
+                            <strong>poeynus</strong>
+                            <br />
+                            네 끝을 볼 열정만 있으면 된다고 생각합니다.
+                            <br />
+                            <small>2020-03-30</small>
+                          </p>
+                        </Content>
+                      </Media.Item>
+                    </Media>
+                  </Media.Item>
+                </Media>
+              </S.MediaBox>
+            </S.CommentWrap>
+          )}
+        </S.PageLeft>
+
         <S.PageRight>
           <S.RightInfo>
             <S.RightPBig>리더 정보</S.RightPBig>
@@ -87,10 +255,12 @@ export const ProjectDetailForm = () => {
             <S.RightPBig>프로젝트 기간</S.RightPBig>
             <S.RightPSmall>22.03.11 ~ 22.09.11</S.RightPSmall>
           </S.RightMid>
+
           <S.RightMid>
             <S.RightPBig>프로젝트 분야</S.RightPBig>
             <S.RightPSmall>이커머스</S.RightPSmall>
           </S.RightMid>
+
           <S.RightFollow>
             <S.RightPBig>프로젝트 구독하기</S.RightPBig>
             <Button.Group align="center">
@@ -101,7 +271,7 @@ export const ProjectDetailForm = () => {
                 size="medium"
                 style={{ width: '100%' }}
               >
-                구독하기
+                구독 쾅!
               </Button>
             </Button.Group>
           </S.RightFollow>
