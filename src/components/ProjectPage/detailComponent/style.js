f1a15553c824f536f230f7fcc2347600814f8287
poeynus <poeynus@gmail.com>
2022-03-30 12:20:33 +0900

    Dev project detail right wrap

--- src/components/ProjectPage/detailComponent/style.js
+++ src/components/ProjectPage/detailComponent/style.js
@@ -4,7 +4,12 @@ export const HeaderWrap = styled.div``;
 
 export const PurposeWrap = styled.div``;
 
-export const HeaderContent = styled.div``;
+export const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 export const ProjectContent = styled.div`
   width: 10rem;
@@ -31,4 +36,54 @@ export const LeaderImg = styled.img`
 
 export const LeaderName = styled.div`
   font-size: 1.4rem;
+  margin-left: 1rem;
+`;
+
+export const StatusBox = styled.div`
+  display: flex;
+  margin-top: 1rem;
+`;
+
+export const PageWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const PageLeft = styled.div`
+  max-width: 840px;
+  box-sizing: border-box;
+`;
+
+export const PageRight = styled.div`
+  max-width: 300px;
+  width: 100%;
+  border: 1px solid #ddd;
+  padding: 20px;
+`;
+
+export const RightInfo = styled.div`
+  padding: 0 0 20px 0;
+  border-bottom: 1px solid #e6e6e6;
+`;
+
+export const RightMid = styled.div`
+  border-bottom: 1px solid #e6e6e6;
+  padding: 20px 0 20px 0;
+  line-height: 1.5;
+  color: #333;
+`;
+
+export const RightFollow = styled.div`
+  padding: 20px 0 20px 0;
+`;
+
+export const RightPBig = styled.p`
+  font-size: 1.3rem;
+  font-weight: bold;
+  margin-bottom: 12px;
+`;
+
+export const RightPSmall = styled.p`
+  font-size: 1rem;
 `;
--- src/components/ProjectPage/projectDetailModule.jsx
+++ src/components/ProjectPage/projectDetailModule.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Container, Heading, Block } from 'react-bulma-components';
+import {
+  Container,
+  Heading,
+  Tag,
+  Media,
+  Image,
+  Content,
+  Icon,
+  Button,
+} from 'react-bulma-components';
 import * as S from './detailComponent/style';
 
 export const ProjectDetailForm = () => {
@@ -9,19 +18,95 @@ export const ProjectDetailForm = () => {
   return (
     <Container>
       <S.HeaderWrap>
-        <S.PurposeWrap>
-          <S.ProjectContent>사이드프로젝트</S.ProjectContent>
-        </S.PurposeWrap>
         <S.HeaderContent>
-          <Heading size={4} style={{ fontWeight: 'bold', fontSize: 34 }}>
+          <S.ProjectContent>사이드프로젝트</S.ProjectContent>
+          <Heading
+            size={4}
+            style={{ fontWeight: 'bold', fontSize: 34, margin: 24 }}
+          >
             웹소설 웹툰 IP 웹 3.0
           </Heading>
           <S.LeaderWrap>
             <S.LeaderImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyGnV38qI3kabyXoa6e9eOn9960Lcnzj3jGA&usqp=CAU" />
-            <S.LeaderName>test</S.LeaderName>
+            <S.LeaderName>배고픈개발자</S.LeaderName>
           </S.LeaderWrap>
+          <S.StatusBox>
+            <Tag color="info" rounded style={{ marginRight: 10 }}>
+              모집 중
+            </Tag>
+            <Tag color="info" className="is-light" rounded>
+              진행 중
+            </Tag>
+          </S.StatusBox>
         </S.HeaderContent>
       </S.HeaderWrap>
+
+      <S.PageWrap>
+        <S.PageLeft>asd</S.PageLeft>
+        <S.PageRight>
+          <S.RightInfo>
+            <S.RightPBig>리더 정보</S.RightPBig>
+            <Media>
+              <Media.Item align="left">
+                <Image
+                  src="http://bulma.io/images/placeholders/128x128.png"
+                  size={64}
+                />
+              </Media.Item>
+              <Media.Item align="center">
+                <Content>
+                  <p>
+                    <strong>배고픈개발자</strong>
+                    <br />
+                    사진 찍는걸 좋아하는 개발자 입니다.
+                    <br />
+                    <small
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                      }}
+                    >
+                      <Icon>
+                        <i className="fas fa-eye" />
+                      </Icon>
+                      <span style={{ display: 'block', marginRight: 20 }}>
+                        1234
+                      </span>
+                      <Icon>
+                        <i className="fas fa-heart" />
+                      </Icon>
+                      <span>64</span>
+                    </small>
+                  </p>
+                </Content>
+              </Media.Item>
+            </Media>
+          </S.RightInfo>
+
+          <S.RightMid>
+            <S.RightPBig>프로젝트 기간</S.RightPBig>
+            <S.RightPSmall>22.03.11 ~ 22.09.11</S.RightPSmall>
+          </S.RightMid>
+          <S.RightMid>
+            <S.RightPBig>프로젝트 분야</S.RightPBig>
+            <S.RightPSmall>이커머스</S.RightPSmall>
+          </S.RightMid>
+          <S.RightFollow>
+            <S.RightPBig>프로젝트 구독하기</S.RightPBig>
+            <Button.Group align="center">
+              <Button
+                className="is-light"
+                color="danger"
+                onClick={() => {}}
+                size="medium"
+                style={{ width: '100%' }}
+              >
+                구독하기
+              </Button>
+            </Button.Group>
+          </S.RightFollow>
+        </S.PageRight>
+      </S.PageWrap>
     </Container>
   );
 };
