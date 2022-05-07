3cce79d6e945f63a609b1031fc63450007dfa391
poeynus <poeynus@gmail.com>
2022-05-07 15:40:20 +0900

    CORS Error

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: `${process.env.REACT_APP_MOON_URL}`,
+      target: `https://4idiot.ddns.net:8080`,
       changeOrigin: true,
     }),
   );
